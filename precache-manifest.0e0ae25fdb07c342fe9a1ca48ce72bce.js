b5bfffd837a96a84ac47084f1450fc22bb11f02a
Nikita Denisov <nikita.denisov.1991@gmail.com>
2020-03-14 15:33:14 +0300

    Updates

---
+++ precache-manifest.0e0ae25fdb07c342fe9a1ca48ce72bce.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f228498e6a14fd7915ae48b95fdfc2f6",
+    "url": "/palettor/index.html"
+  },
+  {
+    "revision": "5c1a621f85ce5c36f716",
+    "url": "/palettor/static/css/main.a4f94a9f.chunk.css"
+  },
+  {
+    "revision": "89f57980ec384f1deaf1",
+    "url": "/palettor/static/js/2.990aa02d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/palettor/static/js/2.990aa02d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c1a621f85ce5c36f716",
+    "url": "/palettor/static/js/main.34b5a323.chunk.js"
+  },
+  {
+    "revision": "e20354a824caedd4f4f3",
+    "url": "/palettor/static/js/runtime-main.eaa0ec68.js"
+  },
+  {
+    "revision": "221385475748334e37a32aca8b7380e2",
+    "url": "/palettor/static/media/close.22138547.svg"
+  },
+  {
+    "revision": "0f4d14252a588c4cd22abc86b8b6b2f4",
+    "url": "/palettor/static/media/list.0f4d1425.svg"
+  },
+  {
+    "revision": "69ed3da694f72226660e7d6149987204",
+    "url": "/palettor/static/media/lock.69ed3da6.svg"
+  },
+  {
+    "revision": "b09efb2e85b0597958d417ef92e4f92e",
+    "url": "/palettor/static/media/unlock.b09efb2e.svg"
+  }
+]);
